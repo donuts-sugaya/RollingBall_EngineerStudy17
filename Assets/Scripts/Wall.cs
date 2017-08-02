6bc75cce33a81ef8aeabbca2f0171ad802051fcc
donuts-sugaya <sugaya.takuma@donuts.ne.jp>
2017-08-02 23:45:29 +0900

    横で動く壁 疾走感を出したかった

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 動く壁
+/// </summary>
+public class Wall : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody rigidbody;
+
+    void Start()
+    {
+        var rBody = rigidbody.velocity;
+        rBody.x = speed;
+        rigidbody.velocity = rBody;
+    }
+}
