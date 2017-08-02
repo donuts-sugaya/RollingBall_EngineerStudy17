f5d1ab8351e034c9440f430c21f2b394e51d2fe4
donuts-sugaya <sugaya.takuma@donuts.ne.jp>
2017-08-02 23:16:02 +0900

    転がって飛ぶ

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private Rigidbody rigidbody;
+
+    [SerializeField] private Transform ground;
+
+    private bool isGround;
+
+    private void Start()
+    {
+        var rBody = rigidbody.velocity;
+        rBody.x = speed;
+        rigidbody.velocity = rBody;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z) && isGround) {
+            // 一定量ジャンプさせる
+            var rBody = rigidbody.velocity;
+            rBody.y = jumpPower;
+            rigidbody.velocity = rBody;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // 地面についた
+        if(other.transform == ground) {
+            isGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        // 地面から離れた
+        if(other.transform == ground) {
+            isGround = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TargetFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ターゲットとの距離を一定に保つ
+/// </summary>
+public class TargetFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private Vector3 distanceVector3;
+
+    private void Start()
+    {
+        distanceVector3 = transform.position - target.position;
+    }
+
+    void Update()
+    {
+        transform.position = target.position + distanceVector3;
+    }
+}
