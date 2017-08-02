7c8b7c9be80b1b3768aefc111049d4220b93e86a
donuts-sugaya <sugaya.takuma@donuts.ne.jp>
2017-08-02 23:27:45 +0900

    uGUIで経過時間追加

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Text timeText;
+
+    private float totalTime;
+    
+    private void Update()
+    {
+        totalTime += Time.deltaTime;
+        timeText.text = "Timer：" + (int)totalTime;
+    }
+}
